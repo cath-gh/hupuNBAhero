f24dfd9ac1faceaf6f1d41a087d739b59611c14d
Cath <cath_r@163.com>
2022-05-27 19:33:01 +0800

    增加原版徽章统计代码

---
+++ aop.js
@@ -0,0 +1,3 @@
+let hurtori = angular.element(document.getElementsByClassName('monthgift-btn')).scope().hurtReceive
+angular.element(document.getElementsByClassName('monthgift-btn')).scope().hurtReceive=function(){console.log('lalalla');hurtori();}
+angular.element(document.getElementsByClassName('monthgift-btn')).scope().hurtReceive()
