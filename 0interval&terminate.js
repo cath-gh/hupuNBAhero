a1f0bb2820f4870b541b2fe6ebe5962eef00141a
Cath <cath_r@163.com>
2022-05-30 19:58:47 +0800

    立即执行首次函数，并在满足条件后终止计时器

---
+++ 0interval&terminate.js
@@ -0,0 +1,15 @@
+// 立即执行首次函数，并在满足条件后终止计时器
+var a = 3; b = 2;
+var func1 = function () { if (a === 3) console.log('done A') };
+var int1 = setInterval((() => {
+    func1();
+    return () => {
+        func1();
+        if (b === 4) {
+            clearInterval(int1); console.log('done B')
+        }
+    }
+})(), 2000);
+
+b = 4;
+clearInterval(int1)
