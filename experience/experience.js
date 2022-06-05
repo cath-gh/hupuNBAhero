7094f177c5af77b9e69fee4ba2d8f7e800fd0449
Cath <cath_r@163.com>
2022-06-05 10:48:39 +0800

    1. 统一token获取 2. 调整部分变量声明

--- experience/experience.js
+++ experience/experience.js
@@ -1,49 +1,43 @@
 // @name         experience
 // @version      0.1
 // @description  NBA英雄 experience
-// @comment      自动完成冠军经理每日历练内容，来源于微信群，在此感谢原作者
+// @author       Cath
+// @update       1. 统一token获取
+// @update       2. 调整部分变量声明
 
-(function (){
+(function () {
     //提示: 请注意设置渠道及区服
-    var serv = 'hupu' // 按需设置渠道，'hupu'=虎扑区, 'wx'=微信区
-    var service = 1 //按需设置区服, 1即代表XX 1区
-    var servURL
-    switch (serv) {
-        case 'hupu':
-            servURL = 'hupu'+`${service==1?'':service}`+'-api.ttnba.cn'
-            break;
-        case 'wx':
-            servURL = 'tt'+`${service==1?'':service}`+'-api.ttnba.cn'
-            break;
-    }
+    let server = 'hupu'; // 按需设置渠道，'hupu'=虎扑区, 'tt'=微信区
+    let service = 1; //按需设置区服, 1即代表XX 1区
+    let servURL = `${server + (service === 1 ? '' : service)}-api.ttnba.cn`;
+    let token = localStorage.TEAM_USER_TOKEN.slice(9, -2); //获取token
 
-    var xmlHttp = new XMLHttpRequest();
-    var date = new Date()
-    var XLnumber = getnumber(servURL)[0]
-    var LLnumber = getnumber(servURL)[1]
+    let xmlHttp = new XMLHttpRequest();
+    let date = new Date();
+    let [XLnumber,LLnumber] = getnumber(servURL);
     if (XLnumber > 0 || LLnumber > 0) {
         for (var i = 1; i < 6; i++) {
-            xmlHttp.open('POST',`https://`+servURL+`/PlayerScoutFight/getArenaDetail?post_time=${date.getTime()}&TEAM_USER_TOKEN=${Object.values(JSON.parse(localStorage.getItem('TEAM_USER_TOKEN')))[0]}&os=m`, false)
+            xmlHttp.open('POST', `https://${servURL}/PlayerScoutFight/getArenaDetail?post_time=${date.getTime()}&TEAM_USER_TOKEN=${token}&os=m`, false)
             xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-            xmlHttp.send('arena_id='+i+'&TEAM_USER_TOKEN=${Object.values(JSON.parse(localStorage.getItem("TEAM_USER_TOKEN")))[0]}')
+            xmlHttp.send('arena_id=' + i + '&TEAM_USER_TOKEN=${token}')
             var o = JSON.parse(xmlHttp.responseText)
             //获取目前各个训练场最高难度
             //console.log(i, o.result.player_level)
             if (i < 5) {
-                if (XLnumber >0 && XLnumber <= 3) { //遍历完成4个训练
+                if (XLnumber > 0 && XLnumber <= 3) { //遍历完成4个训练
                     for (var k = 0; k < XLnumber; k++) {
-                        xmlHttp.open('POST',`https://`+servURL+`/PlayerScoutFight/arenaMatch?post_time=${date.getTime()}&TEAM_USER_TOKEN=${Object.values(JSON.parse(localStorage.getItem('TEAM_USER_TOKEN')))[0]}&os=m`, false)
+                        xmlHttp.open('POST', `https://${servURL}/PlayerScoutFight/arenaMatch?post_time=${date.getTime()}&TEAM_USER_TOKEN=${token}&os=m`, false)
                         xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-                        xmlHttp.send('arena_id='+i+'&detail_id='+parseInt((i-1)*6 + parseInt(o.result.player_level))+'&TEAM_USER_TOKEN=${Object.values(JSON.parse(localStorage.getItem("TEAM_USER_TOKEN")))[0]}')
+                        xmlHttp.send('arena_id=' + i + '&detail_id=' + parseInt((i - 1) * 6 + parseInt(o.result.player_level)) + '&TEAM_USER_TOKEN=${token}')
                     }
                 }
             }
             else { //id=5 完成历练
-                if ( LLnumber > 0) {
-                    for (var j = 0; j < LLnumber; j++){
-                        xmlHttp.open('POST',`https://`+servURL+`/PlayerScoutFight/arenaMatch?post_time=${date.getTime()}&TEAM_USER_TOKEN=${Object.values(JSON.parse(localStorage.getItem('TEAM_USER_TOKEN')))[0]}&os=hupu`, false)
+                if (LLnumber > 0) {
+                    for (var j = 0; j < LLnumber; j++) {
+                        xmlHttp.open('POST', `https://${servURL}/PlayerScoutFight/arenaMatch?post_time=${date.getTime()}&TEAM_USER_TOKEN=${token}&os=hupu`, false)
                         xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-                        xmlHttp.send('arena_id=5'+'&detail_id=' + (parseInt(o.result.curr_detail) + j) + '&TEAM_USER_TOKEN=${Object.values(JSON.parse(localStorage.getItem("TEAM_USER_TOKEN")))[0]}')
+                        xmlHttp.send('arena_id=5' + '&detail_id=' + (parseInt(o.result.curr_detail) + j) + '&TEAM_USER_TOKEN=${token}')
                     }
                 }
             }
@@ -52,17 +46,16 @@
 })()
 
 //获取剩余场次 返回数组[Max, Lilian]: 1-4特训取Max值(Max), 历练之路取剩余值(Lilian) 后续加上当前层数确定爬塔id
-function getnumber(servURL){
+function getnumber(servURL) {
     var xmlHttp = new XMLHttpRequest();
     var date = new Date()
-    xmlHttp.open('POST',`https://`+servURL+`/PlayerScoutFight/arenaIndex?post_time=${date.getTime()}&TEAM_USER_TOKEN=${Object.values(JSON.parse(localStorage.getItem('TEAM_USER_TOKEN')))[0]}&os=m`, false)
+    xmlHttp.open('POST', `https://${servURL}/PlayerScoutFight/arenaIndex?post_time=${date.getTime()}&TEAM_USER_TOKEN=${token}&os=m`, false)
     xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-    xmlHttp.send('TEAM_USER_TOKEN=${Object.values(JSON.parse(localStorage.getItem("TEAM_USER_TOKEN")))[0]}')
-    //console.log(JSON.parse(xmlHttp.responseText))
+    xmlHttp.send('TEAM_USER_TOKEN=${token}')
     var o = JSON.parse(xmlHttp.responseText)
     var LLnumber = 3 - o.result.list[4].num
     var XLnumber
-    switch(date.getDay()) {
+    switch (date.getDay()) {
         case 1:
         case 3:
         case 5:
