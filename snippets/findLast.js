ad08542cc33451b3c0d495287cfc64c871aecd15
Cath <cath_r@163.com>
2022-07-17 17:08:38 +0800

    备份一个正确写法的原型链增加findLast的方法

--- snippets/findLast.js
+++ snippets/findLast.js
@@ -1,25 +1,25 @@
 var a = [{ 'a1': 1, 'a2': 2 }, { 'a1': 1, 'a2': 3 }, { 'a1': 1, 'a2': 4 }, { 'a1': 2, 'a2': 5 }, { 'a1': 2, 'a2': 6 }];
-if (![].findLast) {
-    Array.prototype.findLast = function (arr, callback, thisArg) {
-        for (let index = arr.length - 1; index >= 0; index--) {
-            const value = arr[index];
-            if (callback.call(thisArg, value, index, arr)) {
-                return value;
-            }
-        }
-        return undefined;
+if (!Array.prototype.findLast) {
+  Array.prototype.findLast = function(predicate) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
     }
-}
-a.findLast((item) => { return item.a1 === 1 });
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
 
-// if (![].findLast) {
-//     Array.prototype.findLast = function (arr, callback, thisArg) {
-//         for (let index = arr.length - 1; index >= 0; index--) {
-//             const value = arr[index];
-//             if (callback.call(thisArg, value, index, arr)) {
-//                 return value;
-//             }
-//         }
-//         return undefined;
-//     }
-// }
+    for (var i = length-1; i >=0; i--) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+} 
+a.findLast((item) => { return item.a1 === 1 });
