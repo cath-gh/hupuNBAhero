c67e68aea8b784982d14bcd2fea7cb919220a34d
Cath <cath_r@163.com>
2022-09-04 12:12:12 +0800

    boss code

---
+++ pveBoss/Untitled-1.js
@@ -0,0 +1,58 @@
+{
+    "status": -8409,
+    "message": "当前Boss未开启,请稍候",
+    "server_time": 1662173028,
+    "params": [],
+    "extra_params": []
+}
+
+{
+    "status": 0,
+    "result": {
+        "cp_card_boss_id": "7712",
+        "last_kill_id": 0
+    },
+    "server_time": 1662174001
+}
+
+{
+    "status": -8407,
+    "message": "正在冷却中,无法击杀Boss",
+    "server_time": 1662174013,
+    "params": [],
+    "extra_params": []
+}
+
+{
+    "status": 0,
+    "result": {
+        "cp_card_boss_id": "7712",
+        "last_kill_id": "7181241"
+    },
+    "server_time": 1662174062
+}
+
+{
+    "status": 0,
+    "result": {
+        "cp_card_boss_id": "7712",
+        "last_kill_id": "7181241"
+    },
+    "server_time": 1662174185
+}
+
+{
+    "status": -8407,
+    "message": "正在冷却中,无法击杀Boss",
+    "server_time": 1662174217,
+    "params": [],
+    "extra_params": []
+}
+
+{
+    "status": -8404,
+    "message": "Boss已被击杀",
+    "server_time": 1662174317,
+    "params": [],
+    "extra_params": []
+}
