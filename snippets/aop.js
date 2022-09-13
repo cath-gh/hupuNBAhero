2285a24a82af0909e3bdd92e6a957406cf95d8e6
Cath <cath_r@163.com>
2022-09-13 09:36:03 +0800

    补充aop

--- snippets/aop.js
+++ snippets/aop.js
@@ -2,3 +2,36 @@
 let hurtori = angular.element(document.getElementsByClassName('monthgift-btn')).scope().hurtReceive
 angular.element(document.getElementsByClassName('monthgift-btn')).scope().hurtReceive=function(){console.log('lalalla');hurtori();}
 angular.element(document.getElementsByClassName('monthgift-btn')).scope().hurtReceive()
+
+const AOP = {}
+AOP.before = function (fn, before) {
+    return function() {
+        before.apply(this,arguments)
+        fn.apply(this, arguments)
+    }
+}
+AOP.after = function(fn, after) {
+    return function () {
+        fn.apply(this, arguments)
+        after.apply(this, arguments)
+    }
+}
+
+// 点击按钮提交数据
+function submit() {
+    console.log('提交数据')
+}
+
+document.querySelector('.btn').onclick = submit
+
+// 在原有功能基础上做点装饰：点击按钮，提交数据前做个校验
+function submit() {
+    console.log(this)
+    console.log('提交数据')
+}
+function check() {
+    console.log(this)
+    console.log('先进行校验')
+}
+submit = AOP.before(submit, check)
+document.querySelector('.btn').onclick = submit
