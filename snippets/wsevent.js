9eeed4ac38ea48baffbeca894a07c5ed2b98bc0f
Cath <cath_r@163.com>
2022-09-16 17:28:11 +0800

    添加use方法及测试

--- snippets/wsevent.js
+++ snippets/wsevent.js
@@ -5,3 +5,9 @@ var _send=ws.send.bind(ws);
 ws.send=function(data){var onsend = new CustomEvent('onsend',{detail:data});dispatchEvent(onsend);_send(data);}
 
 ws.onsend=function(e){console.log(e.detail);console.log('end')};
+
+
+var stack={};
+ws.use=function(code, fn){stack[code]=fn};
+ws.onmessage=function(e){var d = JSON.parse(e.data);if(stack.hasOwnProperty(d['msg_id'])){stack[d['msg_id']](d)}}
+ws.send('{"msg_id":200}')
