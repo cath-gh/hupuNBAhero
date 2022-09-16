92a3ca46f5a484fb130a9faeca50a8b8e1fe41ff
Cath <cath_r@163.com>
2022-09-16 10:27:47 +0800

    更合理的逻辑处理

---
+++ snippets/wsevent.js
@@ -0,0 +1,7 @@
+var ws=new WebSocket('ws://82.157.123.54:9010/ajaxchattest');
+ws.onsend=null;
+addEventListener('onsend', (e)=>{if(ws.onsend instanceof Function) {return ws.onsend(e)}});
+var _send=ws.send.bind(ws);
+ws.send=function(data){var onsend = new CustomEvent('onsend',{detail:data});dispatchEvent(onsend);_send(data);}
+
+ws.onsend=function(e){console.log(e.detail);console.log('end')};
