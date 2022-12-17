e098509dd612a777dc217e3ba0a02a1a52b1a27c
Cath <cath_r@163.com>
2022-12-17 15:20:41 +0800

    更新立即执行函数

--- snippets/0interval&terminate.js
+++ snippets/0interval&terminate.js
@@ -1,15 +1,26 @@
 // 立即执行首次函数，并在满足条件后终止计时器
-var a = 3; b = 2;
-var func1 = function () { if (a === 3) console.log('done A') };
-var int1 = setInterval((() => {
-    func1();
-    return () => {
-        func1();
-        if (b === 4) {
-            clearInterval(int1); console.log('done B')
-        }
+var a = 3;
+var b = 4;
+var func1 = function () {
+    console.log('func1 in');
+    if (a === 3) console.log('done A');
+    if (b === 4) {
+        console.log('done B1')
     }
-})(), 2000);
+};
+
+if (b !== 4) {
+    var int1 = setInterval((() => {
+        func1();
+        return () => {
+            console.log('start int');
+            if (b === 4) {
+                clearInterval(int1);
+                console.log('done B1');
+            } else func1();
+        }
+    })(), 2000);
+}
 
 b = 4;
 clearInterval(int1)
