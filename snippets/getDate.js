8f7670cca4212e2b6a2de43f2f22b4c8b0cfa7aa
Cath <cath_r@163.com>
2023-01-28 18:27:17 +0800

    优化代码

--- snippets/getDate.js
+++ snippets/getDate.js
@@ -1,5 +1,17 @@
-var getDate=function(timezone){//获取指定时区的时间
-    var offset_GMT = new Date().getTimezoneOffset(); 
-    var nowDate = new Date().getTime(); 
-    var targetDate = new Date(nowDate + offset_GMT * 60 * 1000 + timezone * 60 * 60 * 1000);return targetDate;
+class _Date extends Date {
+    constructor(...args) {
+        /*方式一
+        var timezone=8;//指定时区
+        var offset_GMT = new Date().getTimezoneOffset(); 
+        var delta = offset_GMT * 60 * 1000 + timezone * 60 * 60 * 1000;
+        */
+
+        //方式二
+        var delta = -3600000//韩国时区，固定偏移
+        if (args.length === 0) {
+            super(new Date().getTime() + delta);
+        } else {
+            super(...args);
+        }
     }
+}
