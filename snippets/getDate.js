93ab57007ba31c3982c1ada3a1762cf2508a3ff3
Cath <cath_r@163.com>
2023-01-30 08:54:10 +0800

    不污染原有方法

--- snippets/getDate.js
+++ snippets/getDate.js
@@ -1,29 +1,10 @@
-// class _Date extends Date {
-//     constructor(...args) {
-//         /*方式一
-//         var timezone=8;//指定时区
-//         var offset_GMT = new Date().getTimezoneOffset(); 
-//         var delta = offset_GMT * 60 * 1000 + timezone * 60 * 60 * 1000;
-//         */
-
-//         //方式二
-//         var delta = -3600000//韩国时区，固定偏移
-//         if (args.length === 0) {
-//             super(new Date().getTime() + delta);
-//         } else {
-//             super(...args);
-//         }
-//     }
-// }
-
 class _Date extends Date {
     constructor(...args) {
         super(...args);
-        var timezone = 7;//指定时区
-        var offset_GMT = new Date().getTimezoneOffset();
-        this._delta = offset_GMT / 60 + timezone;
     }
-    getHours() {
-        return super.getHours() + this._delta;
+    getTimezoneHours(timezone) {//指定时区
+        var offset_GMT = new Date().getTimezoneOffset();
+        var delta = offset_GMT / 60 + timezone;
+        return this.getHours() + delta;
     }
 }
