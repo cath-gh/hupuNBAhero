6243b62b4462bf6aff167a92ef7cbb5755c68193
Cath <cath_r@163.com>
2023-03-19 15:15:06 +0800

    1. 未完成版本

--- src/experience/experience.js
+++ src/experience/experience.js
@@ -1,86 +1,138 @@
 // @name         experience
-// @version      0.11
-// @description  NBA英雄 experience
+// @version      0.2a
+// @description  NBA英雄 冠军经理历练
 // @author       Cath
-// @update       1. 暂时的延时版本
+// @update       1. 未完成版本
 
-(async function () {
-    //提示: 请注意设置渠道及区服
-    let server = 'hupu'; // 按需设置渠道，'hupu'=虎扑区, 'tt'=微信区
-    let service = 1; //按需设置区服, 1即代表XX 1区
-    let servURL = `${server + (service === 1 ? '' : service)}-api.ttnba.cn`;
-    let token = localStorage.TEAM_USER_TOKEN.slice(9, -2); //获取token
+(function () {
+    //#region constant
+    const URLPATH_ARENA_INDEX = '/PlayerScoutFight/arenaIndex';//冠军经理历练索引
+    const URLPATH_GET_ARENA_DETAIL = '/PlayerScoutFight/getArenaDetail';//冠军经理历练详情
+    //#endregion
 
-    let xmlHttp = new XMLHttpRequest();
-    let date = new Date();
-    let [XLnumber, LLnumber] = await getnumber(servURL, token);
-    if (XLnumber > 0 || LLnumber > 0) {
-        for (var i = 1; i < 6; i++) {
-            xmlHttp.open('POST', `https://${servURL}/PlayerScoutFight/getArenaDetail?post_time=${date.getTime()}&TEAM_USER_TOKEN=${token}&os=m`, false)
-            xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-            xmlHttp.send('arena_id=' + i + '&TEAM_USER_TOKEN=${token}')
-            await sleep(850);
-            var o = JSON.parse(xmlHttp.responseText)
-            //获取目前各个训练场最高难度
-            //console.log(i, o.result.player_level)
-            if (i < 5) {
-                if (XLnumber > 0 && XLnumber <= 3) { //遍历完成4个训练
-                    for (var k = 0; k < XLnumber; k++) {
-                        xmlHttp.open('POST', `https://${servURL}/PlayerScoutFight/arenaMatch?post_time=${date.getTime()}&TEAM_USER_TOKEN=${token}&os=m`, false)
-                        xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-                        xmlHttp.send('arena_id=' + i + '&detail_id=' + parseInt((i - 1) * 6 + parseInt(o.result.player_level)) + '&TEAM_USER_TOKEN=${token}')
+    //#region config
+    var server = 'hupu', // 按需设置渠道，'hupu'=虎扑区, 'tt'=微信区
+        service = 1 //按需设置区服, 1即代表XX 1区
+    //#endregion
 
-                        await sleep(950);
-                    }
+    //#region init
+    var date = new Date();
+    var token = localStorage.TEAM_USER_TOKEN.slice(9, -2); //获取token
+    var urlHost = `https://${server + (service === 1 ? '' : service)}-api.ttnba.cn`;
+    var urlArenaIndex = `${urlHost}${URLPATH_ARENA_INDEX}`;
+    var urlGetArenaDetail = `${urlHost}${URLPATH_GET_ARENA_DETAIL}`;
+    //#endregion
+
+    //#region utils
+    // 定义url字符串拼接的方法
+    var concatUrlQuery = function (url, query) {
+        if (query) {
+            let queryArr = [];
+            for (const key in query) {
+                if (query.hasOwnProperty(key)) {
+                    queryArr.push(`${key}=${query[key]}`)
                 }
             }
-            else { //id=5 完成历练
-                if (LLnumber > 0) {
-                    for (var j = 0; j < LLnumber; j++) {
-                        xmlHttp.open('POST', `https://${servURL}/PlayerScoutFight/arenaMatch?post_time=${date.getTime()}&TEAM_USER_TOKEN=${token}&os=hupu`, false)
-                        xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-                        xmlHttp.send('arena_id=5' + '&detail_id=' + (parseInt(o.result.curr_detail) + j) + '&TEAM_USER_TOKEN=${token}')
-
-                        await sleep(950);
-                    }
-                }
+            if (url.indexOf('?') === -1) {
+                url = `${url}?${queryArr.join('&')}`
+            } else if (url.indexOf('=') === -1) {
+                url = `${url}${queryArr.join('&')}`
+            } else {
+                url = `${url}&${queryArr.join('&')}`
             }
         }
+        return url;
     }
-    console.log(`========冠军经理历练Done======`)
-})()
 
-var sleep = async function (time) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(resolve, time)
-    })
-}
-
-//获取剩余场次 返回数组[Max, Lilian]: 1-4特训取Max值(Max), 历练之路取剩余值(Lilian) 后续加上当前层数确定爬塔id
-async function  getnumber(servURL, token) {
-    var xmlHttp = new XMLHttpRequest();
-    var date = new Date()
-    xmlHttp.open('POST', `https://${servURL}/PlayerScoutFight/arenaIndex?post_time=${date.getTime()}&TEAM_USER_TOKEN=${token}&os=m`, false)
-    xmlHttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
-    xmlHttp.send('TEAM_USER_TOKEN=${token}')
-    var o = JSON.parse(xmlHttp.responseText)
-    var LLnumber = 3 - o.result.list[4].num
-    var XLnumber
-    switch (date.getDay()) {
-        case 1:
-        case 3:
-        case 5:
-            XLnumber = 3 - Math.min.apply(null, [o.result.list[0].num, o.result.list[3].num]);
-            break;
-        case 2:
-        case 4:
-        case 6:
-            XLnumber = 3 - Math.min.apply(null, [o.result.list[1].num, o.result.list[2].num]);
-            break;
-        case 0:
-            XLnumber = 3 - Math.min.apply(null, [o.result.list[0].num, o.result.list[1].num, o.result.list[2].num, o.result.list[3].num])
-            break;
+    var getXhr = function (method, url, query, formData) {
+        formData = formData || null;
+        let urlString = concatUrlQuery(url, query);
+        let xmlHttp = new XMLHttpRequest();
+        xmlHttp.open(method, urlString, false);
+        xmlHttp.send(formData);
+        var res = JSON.parse(xmlHttp.responseText);
+        return res;
     }
-    await sleep(950);
-    return [XLnumber, LLnumber]
-}
+
+    var getFetch = async function (method, url, query, formData, delay = 850) {//默认延时850ms
+        formData = formData || null;
+        let urlString = concatUrlQuery(url, query);
+        var res = await fetch(urlString, {
+            method: method,
+            body: formData
+        })
+
+        if (!!delay) {
+            await sleep(delay);
+            log(`操作延时：${delay}`);
+        }
+
+        return res.json();
+    }
+
+    var log = function (value, comment) {
+        comment = comment || '';
+        if (typeof (value) === 'string') {
+            console.info('%c%s - %s: %s', 'color:blue;font-weight:bold', Date().toString(), value, comment);
+        } else {
+            console.info('%c%s : %s', 'color:blue;font-weight:bold', Date().toString(), comment);
+            console.info(value);
+        }
+    }
+
+    var sleep = async function (time) {
+        return new Promise(function (resolve, reject) {
+            setTimeout(resolve, time)
+        })
+    }
+    //#endregion
+
+    //#region method
+    var getArenaIndex = async function () {
+        var method = 'POST';
+        var url = urlArenaIndex;
+        var queryString = {
+            post_time: date.getTime(),
+            TEAM_USER_TOKEN: token,
+            os: 'm'
+        };
+
+        data = {
+            'TEAM_USER_TOKEN': token
+        }
+
+        // var res = getXhr(method, url, queryString, JSON.stringify(data));
+        var res = await getFetch(method, url, queryString, JSON.stringify(data));
+        return res;
+    }
+
+    var getArenaDetail = async function (arenaID) {
+        var method = 'POST';
+        var url = urlGetArenaDetail;
+        var queryString = {
+            post_time: date.getTime(),
+            TEAM_USER_TOKEN: token,
+            os: 'm'
+        };
+
+        data = {
+            arena_id: arenaID,
+            TEAM_USER_TOKEN: token
+        }
+
+        // var res = getXhr(method, url, queryString, JSON.stringify(data));
+        var res = await getFetch(method, url, queryString, JSON.stringify(data));
+        return res;
+    }
+
+
+
+    var taskExperience = async function () {
+        var arenaList = (await getArenaIndex()).result['list'];
+    }
+    //#endregion
+
+    //#region run
+    // taskExperience();
+    //#endregion
+}())
